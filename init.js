ba78ea7df57c04575a420363ede9bf8419ca43fc
subhasree9812 <155650351+subhasree9812@users.noreply.github.com>
2024-08-23 16:29:40 +0530

    Add files via upload

---
+++ init.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+const Chat = require("./models/chat.js");
+
+main()
+.then(()=>{
+    console.log("connection successful");
+})
+.catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+};
+
+let allChats = [
+    {
+    from:"neha",
+    to:"priya",
+    msg:"send me your notes",
+    created_at:new Date(),
+},
+{
+    from:"rohit",
+    to:"mohit",
+    msg:"call me",
+    created_at:new Date(),
+},
+{
+    from:"amit",
+    to:"sruti",
+    msg:"hii",
+    created_at:new Date(),
+},
+{
+    from:"rose",
+    to:"riya",
+    msg:"send me your pic",
+    created_at:new Date(),
+},
+{
+    from:"Subhasree",
+    to:"Neha",
+    msg:"Many Many Happy returns of the day to you, Happy Birthday Dear, May God Bless You!",
+    created_at:new Date(),
+},
+];
+
+Chat.insertMany(allChats);
+
+
+
+
